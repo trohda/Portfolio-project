ce6f1e9996d0c30be361a93c115dc8f2a2bf8af8
Tomasz Rohda <tomaszrohda@wp.pl>
2022-05-24 16:36:08 +0200

    Create content and styles os My Project subsite

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,6 +1,44 @@
 import "./ProjectStyles.css";
-const Project = () => {
-  return <div className="singleProject"></div>;
+const Project = ({ children, title, projectLink, projectText }) => {
+  console.log(projectLink);
+  return (
+    <div className="singleProject">
+      <div className="singleProjectHeaderBox">
+        <div className="singleProjectTopStripe"></div>
+
+        <h2>{title}</h2>
+        <div className="projectScreenshots">{children}</div>
+      </div>
+
+      <div className="singleProjectInfos">
+        <a href={projectLink} target="_blank" rel="noreferrer">
+          {projectLink}
+        </a>
+        <div className="singleProjectInfosBlackCard">
+          <p>
+            {projectText}
+            <br />
+            <br />
+          </p>
+          <span>See more info...</span>
+        </div>
+      </div>
+
+      <div className="spanWrapper">
+        <div className="moreInfoLine2"></div>
+        <div className="moreInfoLine1"></div>
+        <span>
+          Click
+          <br />
+          for
+          <br />
+          More
+          <br />
+          Info
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Project;
--- src/components/Projects/ProjectsList.js
+++ src/components/Projects/ProjectsList.js
@@ -1,11 +1,23 @@
 import "./ProjectsListStyle.css";
 import Project from "../Project/Project";
+import img from "../../img/BestcarwashMini.png";
+import Image from "../Image/Image";
 
 const ProjectsList = () => {
   return (
     <section>
       <div className="projectsList">
-        <Project />
+        <Project
+          title={"Car Wash Page"}
+          projectLink={"https://bestcarwash.netlify.app/"}
+          projectText={`Project created in a group of four persons named: "Kanuni team"`}
+        >
+          <Image
+            src={img}
+            className={"projectsScreenshot"}
+            alt={"Mini best car wash page"}
+          />
+        </Project>
         <Project />
         <Project />
         <Project />
@@ -15,3 +27,4 @@ const ProjectsList = () => {
 };
 
 export default ProjectsList;
+// { title, imgSrc, imgAlt, projectText }
