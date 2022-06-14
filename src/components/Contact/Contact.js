f8357f118c4b9059380f4d5ae884dee965abc2c1
Tomasz Rohda <tomaszrohda@wp.pl>
2022-06-14 21:08:16 +0200

    ContactElement Header styled

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,9 +3,12 @@ import "./ContactStyles.css";
 const Contact = () => {
   return (
     <section className="contactWrapper">
-      <ContactElement topStripeColor={"#007EBB"} />
-      <ContactElement topStripeColor={"black"} />
-      <ContactElement topStripeColor={"rgba(74, 153, 74, 0.9)"} />
+      <ContactElement topStripeColor={"black"} headerText={"Email-Me"} />
+      <ContactElement topStripeColor={"#007EBB"} headerText={"LinkedIn-Me"} />
+      <ContactElement
+        topStripeColor={"rgba(74, 153, 74, 0.9)"}
+        headerText={"Call-Me"}
+      />
     </section>
   );
 };
