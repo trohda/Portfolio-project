d4d51f96d532ba88164c5ce43addb2e5c65da3d2
Tomasz Rohda <tomaszrohda@wp.pl>
2022-06-17 22:22:19 +0200

    footer content and global resposive corretions

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,31 +1,21 @@
+import "./FooterStyles.css";
 const Footer = () => {
   return (
-    <footer
-      style={{
-        width: "100%",
-        height: "100px",
-        position: "relative",
-        textAlign: "center",
-        letterSpacing: "10px",
-        fontSize: "1.5rem",
-        borderTop: "1px solid gray",
-        color: "grey",
-      }}
-    >
-      <p> Footer is Under construction</p>
-
-      <p
-        style={{
-          letterSpacing: "5px",
-          color: "lightgrey",
-          position: "absolute",
-          right: "8%",
-          bottom: "2%",
-          fontSize: "1rem",
-        }}
-      >
-        This page was created using React.js by Tomasz Rohda©️
-      </p>
+    <footer>
+      <div className="footerContentWrapper">
+        <div className="leftContent">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, nisi
+          natus. Corporis dolore accusamus placeat et, consequuntur
+        </div>
+        <div className="rightContent">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+          molestiae laudantium praesentium iusto omnis
+        </div>
+      </div>
+      <div className="copyright">
+        <span> Tomasz Rohda 2022 ©</span>
+        <span> All rights reserved</span>
+      </div>
     </footer>
   );
 };
